96d32b77b98f5f1668222e4a8d9fc373f6e63b8b
Nam-yuuri <76004554+Nam-yuuri@users.noreply.github.com>
2022-10-24 22:38:32 +0700

    Delete backend directory

--- backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDatabase = async () => {
-  
-  await mongoose.connect(process.env.DB_URL);
-  console.log("Đã kết nối đến MongoDB");
-};
-
-module.exports = connectDatabase;
